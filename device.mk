0b3e6d01c01de45e7a61ab92f1ea331f454398c1
Manish Kushwaha <kushwahamanish@google.com>
2024-10-15 14:02:07 +0000

    spes: Migrate sensor HAL to AIDL interface
    Error:
    warning: device/xiaomi/spes/lineage_spes.mk includes non-existent modules in PRODUCT_PACKAGES
    Offending entries:
    android.hardware.sensors@2.1-service.xiaomi-multihal
    
    Bug: 247478991
    Test: tested on-device (P23) ab/P46941077
    Test: `sensor_test -ahal list` and `sensor_test list`
    Test: `sensor_test -ahal sample -s0` and `sensor_test sample -s0`
    
    * Implementation for Xiaomi devices
    * Satisfy FCM
    
    Signed-off-by: clarencelol <clarencekuiek@proton.me>
    Signed-off-by: IHSAN <ihsanulrahman@gmail.com>
    Signed-off-by: tanvirr007 <md.tanvirhasannijum@gmail.com>

--- device.mk
+++ device.mk
@@ -466,7 +466,7 @@ PRODUCT_PACKAGES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@2.0-ScopedWakelock.vendor \
-    android.hardware.sensors@2.1-service.xiaomi-multihal \
+    android.hardware.sensors-service.xiaomi-multihal \
     android.frameworks.sensorservice@1.0 \
     android.frameworks.sensorservice@1.0.vendor \
     libsensorndkbridge
